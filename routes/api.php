15659a326493400d039f6a21570971d3876f678b
aldituhfa <aldituhfa17@gmail.com>
2025-02-16 18:55:30 +0700

    commit

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController; 
+use App\Http\Controllers\AuthController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +22,13 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::apiResource('products', ProductController::class); 
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('auth:sanctum')->group(function () {
+ Route::post('/logout', [AuthController::class, 'logout']);
+ Route::get('/profile', function (Request $request) {
+ return response()->json($request->user());
+ });
+});
+
